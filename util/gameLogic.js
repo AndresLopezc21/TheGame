8d306dbf62174da979a4caf142662895a709f7b8
Andres Castro <acastro@bapcorporativo.com>
2023-10-02 11:24:39 -0700

    Se añade logica de pilas

--- util/gameLogic.js
+++ util/gameLogic.js
@@ -6,13 +6,18 @@ for (let index = 2; index < 99; index++) {
 
 console.log(deck);
 
+
 const sizeHand = 8;
 let playerHand = [];
 let pilaAbajoUno = [],
-  pilaAbajoDos = [],
-  pilaArribaUno = [],
-  pilaArribaDos = [];
+    pilaAbajoDos = [],
+    pilaArribaUno = [],
+    pilaArribaDos = [];
 
+ pilaArribaUno[0] = 1;
+ pilaArribaDos[0] = 1;
+ pilaAbajoUno[0] = 100;
+ pilaAbajoDos[0] = 100;
 function llenarManoInicial() {
   while (playerHand.length < sizeHand && deck.length > 0) {
     var indiceAleatorio = Math.floor(Math.random() * deck.length);
@@ -62,20 +67,44 @@ function tomarCartaAleatoria() {
 function pushAPila(pila, cartaHand) {
   switch (pila) {
     case 1:
-      pilaArribaUno.unshift(cartaHand);
-      let pilaArriba1 = document.getElementById("pilaArribaUno");
-      pilaArriba1.innerHTML = pilaArribaUno[0];
-      let i = playerHand.findIndex((item) => item.id === cartaHand.id);
-      console.log(i);
+        console.log(cartaHand);
+        console.log(pila);
+        let cartaAnterior = pilaArribaUno[0];
+        pilaArribaUno.unshift(cartaHand);
+        if(cartaAnterior > cartaHand){
+             console.log("ese we");
+             console.log("carta en el tope de la pila  a cambiar"+ pilaArribaUno[0]);
+             console.log("carta que salio de la mano a la pila: " + cartaHand);
+             pilaArribaUno.unshift(cartaAnterior);
+             break;
+        }
+          let pilaArriba1 = document.getElementById("pilaArribaUno");
+          pilaArriba1.innerHTML = pilaArribaUno[0];
+          let i = playerHand.findIndex((item) => item.id === cartaHand.id);
+          console.log("index: " + i);
+          console.log("carta en el tope de la pila  a cambiar"+ pilaArribaUno[0]);
+          console.log("carta que salio de la mano a la pila: " + cartaHand);
+          if (i !== -1) {
+            playerHand.splice(i, 1);
+          }
+          console.log("mano del jugardor: " + playerHand);
+      break;
+
+    case 2:
       console.log(cartaHand);
-      if (i !== -1) {
-        playerHand.splice(i, 1);
+        console.log(pila);
+      pilaArribaDos.unshift(cartaHand);
+      let pilaArriba2 = document.getElementById("pilaArribaDos");
+      pilaArriba2.innerHTML = pilaArribaDos[0];
+      let j = playerHand.findIndex((item) => item.id === cartaHand.id);
+      console.log(j);
+      console.log(cartaHand);
+      if (j !== -1) {
+        playerHand.splice(j, 1);
       }
       console.log(playerHand);
       break;
-    case 2:
-      pilaAbajoDos;
-      break;
+
     case 3:
       pilaArribaUno;
       break;
