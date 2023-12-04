4a81b6664a9311a548536e6484a1aa1837d0bf97
Andres Castro <acastro@bapcorporativo.com>
2023-12-03 23:09:22 -0700

    reglas

--- util/gameLogic.js
+++ util/gameLogic.js
@@ -1,4 +1,4 @@
-/*const deck = Array.from({ length: 98 }, (_, index) => index + 2);
+const deck = Array.from({ length: 98 }, (_, index) => index + 2);
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -7,8 +7,7 @@ function shuffleArray(array) {
   }
 }
 
-shuffleArray(deck);*/
-deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+shuffleArray(deck);
 console.log(deck);
 
 const sizeHand = 8;
