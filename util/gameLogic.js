f2296c7a4640d406e36c7e139e1bc27d5b75d39b
Andres Castro <acastro@bapcorporativo.com>
2023-12-03 21:09:28 -0700

    funcionlidad, juego y victoria listos

--- util/gameLogic.js
+++ util/gameLogic.js
@@ -1,4 +1,4 @@
-const deck = Array.from({ length: 98 }, (_, index) => index + 2);
+/*const deck = Array.from({ length: 98 }, (_, index) => index + 2);
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -7,8 +7,8 @@ function shuffleArray(array) {
   }
 }
 
-shuffleArray(deck);
-
+shuffleArray(deck);*/
+deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(deck);
 
 const sizeHand = 8;
@@ -19,6 +19,7 @@ playerHand = [];
   (pilaArribaDos = []);
 
 cartasArrojadas = 0;
+scoreJugador = 98;
 pilaArribaUno[0] = 1;
 pilaArribaDos[0] = 1;
 pilaAbajoUno[0] = 100;
@@ -33,8 +34,6 @@ function actualizarMano(playerHand) {
   let carta6 = document.getElementById("hand6");
   let carta7 = document.getElementById("hand7");
   let carta8 = document.getElementById("hand8");
-
-  playerHand = _.orderBy(playerHand);
 
   carta1.innerHTML = playerHand[0];
   carta2.innerHTML = playerHand[1];
@@ -56,114 +55,110 @@ function llenarManoInicial() {
   console.log("deck despues de repartir al jugador: " + deck);
 
   actualizarMano(playerHand);
+  playerHand = _.orderBy(playerHand);
 
   let botonComenzarBack = document.getElementById("botonComenzar");
 
   botonComenzarBack.style.display = "none";
+
+  verificarVictoria();
 }
 
 function pushAPila(pila, cartaHand) {
+  let cartaAnterior, cartaMano;
+
   switch (pila) {
     case 1:
-      console.log(cartaHand);
-      console.log(pila);
       cartaAnterior = pilaArribaUno[0];
       cartaMano = Number(cartaHand);
-      pilaArribaUno.unshift(cartaMano);
-      if (cartaAnterior < cartaMano || cartaMano == cartaAnterior - 10) {
-        let pilaArriba1 = document.getElementById("pilaArribaUno");
-        pilaArriba1.innerHTML = pilaArribaUno[0];
-        let i = playerHand.findIndex((item) => item.id === cartaMano.id);
-        if (i !== -1) {
-          playerHand.splice(i, 1);
-        }
-        cartasArrojadas++;
-        break;
+
+      if (cartaAnterior < cartaMano || cartaMano === cartaAnterior - 10) {
+        pilaArribaUno.unshift(cartaMano);
+        actualizarPilaYMano(pilaArribaUno, "pilaArribaUno");
+      } else {
+        console.log("Ese we");
+        pilaArribaUno.unshift(cartaAnterior);
       }
-      console.log("Ese we");
-      pilaArribaUno.unshift(cartaAnterior);
       break;
 
     case 2:
-      console.log(cartaHand);
-      console.log(pila);
       cartaAnterior = pilaArribaDos[0];
       cartaMano = Number(cartaHand);
-      pilaArribaDos.unshift(cartaMano);
-      if (cartaAnterior < cartaMano || cartaMano == cartaAnterior - 10) {
-        let pilaArriba2 = document.getElementById("pilaArribaDos");
-        pilaArriba2.innerHTML = pilaArribaDos[0];
-        let i = playerHand.findIndex((item) => item.id === cartaMano.id);
-        if (i !== -1) {
-          playerHand.splice(i, 1);
-        }
-        cartasArrojadas++;
-        break;
+
+      if (cartaAnterior < cartaMano || cartaMano === cartaAnterior - 10) {
+        pilaArribaDos.unshift(cartaMano);
+        actualizarPilaYMano(pilaArribaDos, "pilaArribaDos");
+      } else {
+        console.log("Ese we");
+        pilaArribaDos.unshift(cartaAnterior);
       }
-      console.log("Ese we");
-      pilaArribaDos.unshift(cartaAnterior);
       break;
 
     case 3:
-      console.log(cartaHand);
-      console.log(pila);
       cartaAnterior = pilaAbajoUno[0];
       cartaMano = Number(cartaHand);
-      pilaAbajoUno.unshift(cartaMano);
-      if (cartaAnterior > cartaMano || cartaMano == cartaAnterior + 10) {
-        let pilaAbajo1 = document.getElementById("pilaAbajoUno");
-        pilaAbajo1.innerHTML = pilaAbajoUno[0];
-        let k = playerHand.findIndex((item) => item.id === cartaMano.id);
-        if (k !== -1) {
-          playerHand.splice(k, 1);
-        }
-        console.log(playerHand);
-        cartasArrojadas++;
-        break;
+
+      if (cartaAnterior > cartaMano || cartaMano === cartaAnterior + 10) {
+        pilaAbajoUno.unshift(cartaMano);
+        actualizarPilaYMano(pilaAbajoUno, "pilaAbajoUno");
+      } else {
+        console.log("Ese we");
+        pilaAbajoUno.unshift(cartaAnterior);
       }
-      console.log("Ese we");
-      pilaAbajoUno.unshift(cartaAnterior);
       break;
+
     case 4:
-      console.log(cartaHand);
-      console.log(pila);
       cartaAnterior = pilaAbajoDos[0];
       cartaMano = Number(cartaHand);
-      pilaAbajoDos.unshift(cartaMano);
-      if (cartaAnterior > cartaMano || cartaMano == cartaAnterior + 10) {
-        let pilaAbajo2 = document.getElementById("pilaAbajoDos");
-        pilaAbajo2.innerHTML = pilaAbajoDos[0];
-        let l = playerHand.findIndex((item) => item.id === cartaMano.id);
-        if (l !== -1) {
-          playerHand.splice(l, 1);
-        }
-        console.log(playerHand);
-        cartasArrojadas++;
-        break;
+
+      if (cartaAnterior > cartaMano || cartaMano === cartaAnterior + 10) {
+        pilaAbajoDos.unshift(cartaMano);
+        actualizarPilaYMano(pilaAbajoDos, "pilaAbajoDos");
+      } else {
+        console.log("Ese we");
+        pilaAbajoDos.unshift(cartaAnterior);
       }
-      console.log("Ese we");
-      pilaAbajoDos.unshift(cartaAnterior);
       break;
+  }
+}
+
+function actualizarPilaYMano(pila, elementoId) {
+  let pilaElemento = document.getElementById(elementoId);
+  pilaElemento.innerHTML = pila[0];
+
+  let i = playerHand.findIndex((item) => item && item === pila[0]);
+  if (i !== -1) {
+    playerHand.splice(i, 1);
+    console.log(playerHand);
+    cartasArrojadas++;
+    scoreJugador--;
   }
 }
 
 function pasarTurno() {
   if (cartasArrojadas >= 2) {
     while (playerHand.length < sizeHand && deck.length > 0) {
-      var indice = deck[0];
-      var carta = deck.splice(indice, 1)[0];
-      playerHand.push(carta);
-      console.log("Tu mano actual: " + playerHand);
-      actualizarMano(playerHand);
+      if (deck.length > 0) {
+        var indice = deck[0];
+        var carta = deck.splice(indice, 1)[0];
+        playerHand.push(carta);
+      }
     }
+
+    console.log("Tu mano actual: " + playerHand);
+    actualizarMano(playerHand);
     cartasArrojadas = 0;
+    verificarVictoria();
   } else {
-    mostrarMensajeFinTurno(
-      "Necesitas arrojar al menos dos cartas antes de pasar el turno"
-    );
+    const check = verificarVictoria();
+    if (!check) {
+    } else {
+      mostrarMensajeFinTurno(
+        "Necesitas arrojar al menos dos cartas antes de pasar el turno"
+      );
+    }
   }
 }
-
 function pasarTurnoDificil() {
   if (cartasArrojadas >= 2) {
     cartasArrojadas = 0;
@@ -187,4 +182,12 @@ function mostrarMensajeFinTurno(mensaje) {
 
 function mostrarMensajeFinTurnoDificil(mensaje) {
   alert(mensaje);
+}
+
+function verificarVictoria() {
+  const todasCartasUndefined = playerHand.every((carta) => carta === undefined);
+
+  if ((deck.length === 0 && playerHand.length === 0) || todasCartasUndefined) {
+    alert("¡Felicidades, has ganado!");
+  }
 }
